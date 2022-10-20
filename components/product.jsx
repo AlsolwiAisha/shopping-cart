8bc037654da1a637cd3c57527cb651a875e26d60
AlsolwiAisha <aisha.alsolwi@gmail.com>
2022-10-20 22:23:40 +0300

    Update product.jsx

--- components/product.jsx
+++ components/product.jsx
@@ -1,18 +1,47 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Cart from './cart';
+import { useRouter } from 'next/router'
 const Product = ({products}) => {
-  return (
+  const [cart,setCart]=useState([]);
+ const[cartnum,setCartnum]=useState(0);
+ const [total,setTotal]=useState(0);
+function addcart(img,price,title){
+  setTotal(total+price)
+  setCartnum(cartnum+1)
+   return (
+       setCart([...cart,{image:{img},price:{price},title:{title}}])
+     
+   )
+}
+function carts(){ }
+console.log(total)
+  return( <>
+   <div className='allPro'>
+        {products.map((products) => {
+           return (
+               <div className='proInfo' >
+                 <img  src={products.image} alt="" />
+                 <div className='proTitle'>{products.title}</div>
+                 <div>{products.price} $</div>
+                 <div>{products.category}</div>
+                 <button className='btn' onClick={()=>{addcart(products.image  ,products.price,products.title );carts()}} 
+                >Add to card</button>
+                </div>
+           );
+         })}
+      
+  
+   </div>
+   <div className="carts">
+    <div><Cart cart={cart}/></div> 
     <div>
-         {products.map((products) => {
-            return (
-              <div className='proInfo' >
-               <div>{products.id}</div> 
-               <img  src={products.image} alt="" />
-              </div>
-            );
-          })}
+     <p>cart: {cartnum} </p>   
+     <p>total={total}$</p>
+     <button> Pay</button>
     </div>
-  )
+
+ </div>
+   </>)
 }
 
 export default Product
