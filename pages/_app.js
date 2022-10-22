cb1d99fd66ef4e8edfe4f499caf3021340d3bca5
AlsolwiAisha <aisha.alsolwi@gmail.com>
2022-10-22 18:40:36 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 
 import Layout from '../components/layout'
 import '../styles/globals.css'
+import deleteFromArray from 'delete-from-array'
 
 function MyApp({ Component, pageProps }) {
   return (
