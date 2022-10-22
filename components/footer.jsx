121d8777b10ff75c2fe20bb18501b63778944c36
AlsolwiAisha <aisha.alsolwi@gmail.com>
2022-10-22 21:49:55 +0300

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import Image from 'next/image'
+import img from '../public/4.png'
+import img2 from '../public/5.png'
+import git from '../public/github.png'
 const Footer = () => {
   return (
-    <div className='footer'>Footer</div>
+    <div className='footer'>
+        <Image className='logo' src={img}  layout="fixed" width= {30}
+              height= {30}/>
+     <Image className='logo' src={git} layout="fixed" width= {30}
+              height= {30}/>
+    <Image className='logo' src={img2} layout="fixed" width= {30}
+              height= {30}/>
+   
+    </div>
   )
 }
 
