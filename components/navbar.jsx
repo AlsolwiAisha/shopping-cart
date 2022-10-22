a5cada2f23e7508c63288f82f7b555fb1e5278be
AlsolwiAisha <aisha.alsolwi@gmail.com>
2022-10-22 21:49:59 +0300

    Update navbar.jsx

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Link from 'next/link'
-
+import Image from 'next/image'
+import img from '../public/shopping-cart.png'
 const Navbar = () => {
   return (
     <div className='nav'>
+      <Image className='logo' src={img} layout="fixed" width= {50}
+              height= {50}/>
+      <div className='navLink'>
       <Link href="/"> home</Link>   
-      <Link href="/about"> about</Link>   
-
+      {/* <Link href="/about"> about</Link>    */}
+</div>
  </div>
   )
 }
