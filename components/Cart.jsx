86b81abc5515a0595cf7574a5f003086f98dcadb
AlsolwiAisha <aisha.alsolwi@gmail.com>
2022-10-22 21:49:50 +0300

    Update Cart.jsx

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,16 +1,15 @@
 import React, { useState } from 'react'
-import update from 'immutability-helper'
 import Image from 'next/image'
 const Cart = ({cart}) => {
  
   return (
-    <div >
-      your Cart is:
+    <div className='cart'>
+     <h2>your Cart is:</h2> 
        {cart.map((crt) => {
            return (
         <div className='cartMain'>
             <Image src={crt.image.img} alt={crt.title.title} layout="fixed" width= {30}
-  height= {30} />
+              height= {30} />
             <div>{crt.price.price}</div>
             <div className='proTitle'>{crt.title.title}</div>
             {/* <button onClick={()=>{removeItem({image:{img},price:{price},title:{title}})}} >del</button> */}
