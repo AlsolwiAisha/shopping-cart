729e72ddfb4900ab5a7c26c5b04808045d8e2fe9
AlsolwiAisha <aisha.alsolwi@gmail.com>
2022-10-26 22:26:26 +0300

    Update Cart.jsx

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -7,14 +7,13 @@ const Cart = ({cart}) => {
        {cart.map((crt) => {
            return (
         <div className='cartMain'>
-          <div key={crt.id.id}>
             <Image src={crt.image.img} alt={crt.title.title} layout="fixed" width= {30}
               height= {30} />
             <div>{crt.price.price}</div>
             <div className='proTitle'>{crt.title.title}</div>
             {/* <button onClick={()=>{removeItem({image:{img},price:{price},title:{title}})}} >del</button> */}
        </div>
-        </div>)}
+       )}
         )}
        
      </div>
