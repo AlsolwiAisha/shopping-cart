be7e76e8ea72609234179d3d9b4a11d0878e1b8a
AlsolwiAisha <aisha.alsolwi@gmail.com>
2022-10-26 22:04:34 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   images: {
     domains: ['fakestoreapi.com'],
   },
+   eslint: {
+    dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
 }
 
 module.exports = nextConfig
