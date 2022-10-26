7b838c2c86afc1b658fa4fc3ffaa5a71d4f2c60e
AlsolwiAisha <aisha.alsolwi@gmail.com>
2022-10-26 22:22:08 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ const nextConfig = {
    eslint: {
     dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
   },
+  extends: "next",
+  rules: {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off"
+  }
 }
 
 module.exports = nextConfig
