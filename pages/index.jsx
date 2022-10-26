3aee9d298ce518cb5af0a47169d26788480237a0
AlsolwiAisha <aisha.alsolwi@gmail.com>
2022-10-26 22:34:14 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { useEffect, useState } from 'react';
 import Product from '../components/product';
-import Cart from '../components/cart';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
